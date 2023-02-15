06a46d68bc12b33c95c622521d5c46620452bcf2
rtungol <rarrtungol@gmail.com>
2023-02-15 18:36:56 -0500

    spring mvc for Reviews complete

---
+++ src/main/java/com/ralphtungol/movies/ReviewService.java
@@ -0,0 +1,25 @@
+package com.ralphtungol.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    // used for more complex function that should not implemented in repositories.
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class) //performs update call on movie class
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        return  review;
+    }
+}
